58faf035840db4898d3bc9cf9fd1bc54080bc4c7
QuentinBacuet <quentin.bacuet@epfl.ch>
2017-12-12 21:44:21 +0100

    Add vertical and horizontal data augmentation

--- utils/image_augmentation.py
+++ utils/image_augmentation.py
@@ -13,21 +13,60 @@ output_path_truth = '../data/training/groundtruth/'
 output_path_images = '../data/training/images/'
 
 
-def generate_rotated_training_images(angle=45, use_delta=False, override_image=True):
-    num_files = 100 if override_image else len(next(os.walk(output_path_truth))[2])
+def generate_images(path_input, path_output, transformation, *params):
+    img = cv2.imread(path_input)
+    cv2.imwrite(path_output, transformation(img, *params))
 
+
+def generate_rotated_training_images(angle=45, use_delta=False, override_image=True):
+    num_files = 0 if override_image else len(next(os.walk(output_path_truth))[2])
     for i in range(1, 101):
         delta = random.uniform(-5, 5) if use_delta else 0
-        generate_rotated_image(input_path_truth + 'satImage_' + '{num:03d}'.format(num=i) + '.png',
-                               output_path_truth + 'satImage_' + '{num:03d}'.format(num=i + num_files) + '.png',
-                               True, angle + delta)
-        generate_rotated_image(input_path_images + 'satImage_' + '{num:03d}'.format(num=i) + '.png',
-                               output_path_images + 'satImage_' + '{num:03d}'.format(num=i + num_files) + '.png',
-                               False, angle + delta)
+        generate_images(input_path_truth + 'satImage_' + '{num:03d}'.format(num=i) + '.png',
+                        output_path_truth + 'satImage_' + '{num:03d}'.format(num=i + num_files) + '.png',
+                        rotate_image, True, angle + delta)
+        generate_images(input_path_images + 'satImage_' + '{num:03d}'.format(num=i) + '.png',
+                        output_path_images + 'satImage_' + '{num:03d}'.format(num=i + num_files) + '.png',
+                        rotate_image, False, angle + delta)
 
 
-def generate_rotated_image(path_input, path_output, use_grayscale, angle=45):
-    img = cv2.imread(path_input)
+def generate_vertical_flip_training_images(override_image=True):
+    num_files = 0 if override_image else len(next(os.walk(output_path_truth))[2])
+    for i in range(1, 101):
+        generate_images(input_path_truth + 'satImage_' + '{num:03d}'.format(num=i) + '.png',
+                        output_path_truth + 'satImage_' + '{num:03d}'.format(num=i + num_files) + '.png',
+                        vertical_flip, True)
+        generate_images(input_path_images + 'satImage_' + '{num:03d}'.format(num=i) + '.png',
+                        output_path_images + 'satImage_' + '{num:03d}'.format(num=i + num_files) + '.png',
+                        vertical_flip, False)
+
+
+def generate_horizontal_flip_training_images(override_image=True):
+    num_files = 0 if override_image else len(next(os.walk(output_path_truth))[2])
+    for i in range(1, 101):
+        generate_images(input_path_truth + 'satImage_' + '{num:03d}'.format(num=i) + '.png',
+                        output_path_truth + 'satImage_' + '{num:03d}'.format(num=i + num_files) + '.png',
+                        horizontal_flip, True)
+        generate_images(input_path_images + 'satImage_' + '{num:03d}'.format(num=i) + '.png',
+                        output_path_images + 'satImage_' + '{num:03d}'.format(num=i + num_files) + '.png',
+                        horizontal_flip, False)
+
+
+def vertical_flip(img, use_grayscale):
+    res = cv2.flip(img, 1)
+    if use_grayscale:
+        res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    return res
+
+
+def horizontal_flip(img, use_grayscale):
+    res = cv2.flip(img, 0)
+    if use_grayscale:
+        res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    return res
+
+
+def rotate_image(img, use_grayscale, angle):
     num_rows, num_cols = img.shape[:2]
     black_image = np.zeros((num_rows, num_cols, 3), np.uint8)
 
@@ -49,7 +88,7 @@ def generate_rotated_image(path_input, path_output, use_grayscale, angle=45):
     if use_grayscale:
         crop_image = cv2.cvtColor(crop_image, cv2.COLOR_BGR2GRAY)
 
-    cv2.imwrite(path_output, crop_image)
+    return crop_image
 
 
 if __name__ == '__main__':
@@ -59,3 +98,5 @@ if __name__ == '__main__':
     generate_rotated_training_images(90, use_delta=False, override_image=False)
     generate_rotated_training_images(180, use_delta=False, override_image=False)
     generate_rotated_training_images(270, use_delta=False, override_image=False)
+    generate_vertical_flip_training_images(override_image=False)
+    generate_horizontal_flip_training_images(override_image=False)
